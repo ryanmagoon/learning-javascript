6242cc9a5b59dd30e773398b7433d72624d74b75
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-04 12:29:47 -0400

    setup eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 
-gulp.task('default', function() {
+gulp.task('default', function () {
+    // Run ESLint
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
     // Node source
     gulp.src("es6/**/*.js")
         .pipe(babel())
