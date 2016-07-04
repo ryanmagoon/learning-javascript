5232bf6a8428f96476388893653ed9b20a381125
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-04 11:00:53 -0400

    say hello to the world

--- main.js
+++ main.js
@@ -3,20 +3,13 @@ $(document).ready(function() {
   paper.install(window);
   paper.setup(document.getElementById('mainCanvas'));
 
-  // var c = Shape.Circle(200, 200, 50);
-  // c.fillColor = 'green';
-  // c.strokeColor = 'orange';
-  // c.strokeWidth = '3';
-
-  // var c;
-  // for(var x=25; x<400; x+=50) {
-  //   for(var y=25; y<400; y+=50) {
-  //     c = Shape.Circle(x, y, 20);
-  //     c.fillColor = 'green';
-  //     c.strokeColor = 'orange';
-  //     c.strokeWidth = '3';
-  //   }
-  // }
+  var c = Shape.Circle(200, 200, 80);
+  c.fillColor = 'black';
+  var text = new PointText(200, 200);
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.fontSize = 20;
+  text.content = 'hello world';
  
   var tool = new Tool();
 
