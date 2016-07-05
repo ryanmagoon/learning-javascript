19d91a1a5e15b0bd4d92d16d71746dbfbffba1d5
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-04 20:31:35 -0400

    add helper functions for gambling

---
+++ es6/helperFunctions.js
@@ -0,0 +1,10 @@
+// returns a random integer in the range [m, n] (inclusive)
+function rand(m,n) {
+    return m + Math.floor((n - m + 1) * Math.random());
+}
+
+// randomly returns a string representing one of the six
+// Crown and Anchor faces
+function randFace() {
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0,5)];
+}
